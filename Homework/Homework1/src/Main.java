720cb87ee481aa33772b3fca2a47c60692d4c9a5
Lucanb <66071357+Lucanb@users.noreply.github.com>
2023-02-23 12:17:01 +0200

    ValidareFunctie

--- Homework/Homework1/src/Main.java
+++ Homework/Homework1/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Pleas enter an integer bigger that 0");
+        int myint = keyboard.nextInt();
+          if(myint <=0 )
+          {
+              System.out.println("Not a valide number");
+          }
+              else
+        {
+              TransformareMatrice.Write();
+        }
+    }
+}
+
+class TransformareMatrice{
+    public static void Write()
+    {
+        System.out.println("I like apples");
     }
 }
