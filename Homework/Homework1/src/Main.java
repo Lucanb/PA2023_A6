c8e3c200dfa1b4dddfe8730bffe2bbd9c7c63d78
Lucanb <66071357+Lucanb@users.noreply.github.com>
2023-02-23 14:33:13 +0200

    23.02.2023
    Cum arata acum la final ora 14:33

--- Homework/Homework1/src/Main.java
+++ Homework/Homework1/src/Main.java
@@ -1,23 +1,64 @@
 import java.util.Scanner;
+import java.time.*;
+
 public class Main {
+
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Pleas enter an integer bigger that 0");
-        int myint = keyboard.nextInt();
-          if(myint <=0 )
-          {
-              System.out.println("Not a valide number");
-          }
-              else
-        {
-              TransformareMatrice.Write();
+        int n = keyboard.nextInt();
+        if (n <= 0) {
+            System.out.println("Not a valide number");
+        } else if (n < 30000) {
+            int M[][] = new int[n][n];
+            ;
+            TransformareMatrice.CreeateMatrix(M, n);
+            TransformareMatrice.Afisare(M, n);
+        } else {
+            //Aici este necesar numai timpul rularii aplicatiei;
+            int M[][] = new int[n][n];
+            TransformareMatrice T = new TransformareMatrice();
+            T.CreeateMatrix(M, n);
+            T.AfisareTimp();
         }
+
     }
 }
 
-class TransformareMatrice{
-    public static void Write()
-    {
-        System.out.println("I like apples");
+class TransformareMatrice {
+    //Clar ca matricea o vom creea pe baza permutariilor unei linii(fac o matrice circulara)
+
+    //  static const time = Date;
+    static Clock clock = Clock.systemDefaultZone();
+    static long time;
+
+    public TransformareMatrice() {
+        long time = clock.millis();
+    }
+
+    public static void CreeateMatrix(int M[][], int n) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
+                if ((i + j + 1) % n != 0)
+                    M[i][j] = (i + j + 1) % n;
+                else
+                    M[i][j] = n;
+            }
+        time = clock.millis() - time;
+    }
+
+    public static void Afisare(int M[][], int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(M[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void AfisareTimp() {
+        System.out.print(time);
+
     }
 }
