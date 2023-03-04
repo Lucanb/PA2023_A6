d1a23baeb12ba8f3a3150efbc7d73abb11dcd548
Lucanb <66071357+Lucanb@users.noreply.github.com>
2023-03-04 12:41:19 +0200

    Done it

--- Seminar2/Bonus2/src/Map.java
+++ Seminar2/Bonus2/src/Map.java
@@ -7,8 +7,8 @@ public class Map {
     int roadsNumber;
 
     //Vect de Roads-uri ,vect de Locations; (pt verificarea lor) ~ a list
-     Vector<Road> roadVector = new Vector<>(500);
-     Vector<Location> locationVector = new Vector<>(500);
+    Vector<Road> roadVector = new Vector<Road>(500);
+    Vector<Location> locationVector = new Vector<Location>(500);
     //matrice de coorodonate cu 1 sau 0 cost(daca am drum)
 
     //Vect cu id intre orase (il iau de la 0 la n); si pe matrice la fel voi avea exact dependenta de id-uri.
@@ -17,73 +17,76 @@ public class Map {
 
 
     //adaug operatori de supra incarcare(addRoad,addCity)
-    public void addRoad(Road road){
-       for(int i=0;i<roadsNumber;i++)
-           if(road.equals(roadVector.get(i)))
-           {
-              System.out.println("Error,this road exists!");
-           }
-       else{
-              if(road.getLength()>=road.distance(road.getFirstCity(),road.getSecondCity())) {
-                  int x=-1, y=-1; //Trebuie sa vad in lista
-                  for(int j=0;j<locationVector.size();j++)
-                  {
-                         if(locationVector.get(j).getCoord() == road.getFirstCity())
-                             x=j;
-                         if(locationVector.get(j).getCoord() == road.getSecondCity())
-                             y=j;
-
-                  }
-                  if(x!=-1 && y!= -1) {
-                      roadVector.add(road);
-                      costMatrix[x][y] = road.getLength();
-                      costMatrix[y][x] = road.getLength();
-                      roadsNumber++;
-                  }else
-                  {
-                      System.out.println("A city doe not exists!");
-                  }
-
-              }
-              else
-              {
-                  System.out.println("Wrong Road Distance");
-              }
-           }
-    }
-    public void addCity(Location location){
-        for(int i=0;i<numberCities;i++)
-            if(location.equals(locationVector.get(i)))
-            {
+    public void addRoad(Road road) {
+        int sw = 0;
+        for (int i = 0; i < roadsNumber; i++)
+            if (road.equals(roadVector.get(i))) {
                 System.out.println("Error,this road exists!");
+                sw = 1;
             }
-            else
-            {
-                vectId[numberCities]=numberCities;
-                locationVector.add(location);
-                numberCities++;
+        if (sw == 0) {
+            if (road.getLength() >= road.distance(road.getFirstCity(), road.getSecondCity())) {
+                int x = -1, y = -1; //Trebuie sa vad in lista
+                for (int j = 0; j < locationVector.size(); j++) {
+                    if (locationVector.get(j).getCoord() == road.getFirstCity())
+                        x = j;
+                    if (locationVector.get(j).getCoord() == road.getSecondCity())
+                        y = j;
+
+                }
+                if (x != -1 && y != -1) {
+                    roadVector.add(road);
+                    costMatrix[x][y] = road.getLength();
+                    costMatrix[y][x] = road.getLength();
+                    roadsNumber++;
+                } else {
+                    System.out.println("A city doe not exists!");
+                }
+
+            } else {
+                System.out.println("Wrong Road Distance");
             }
+        } else {
+            System.out.println("Canno t inser the road");
+        }
     }
-    public void PossibleCity(Location locationStart,Location locationFinish){
-          for(int i=0;i<roadVector.size();i++)
-              if(locationStart.equals(roadVector.get(i).getFirstCity()) && locationFinish.equals(roadVector.get(i).getSecondCity()))
-              {
-                System.out.println("It exists such a road from the 2 cities!");
-              }
-              else
-              {
-                  System.out.println("It doesn't exists such a road from the 2 cities!");
-              }
+
+    public void addCity(Location location) {
+        int sw = 0;
+        for (int i = 0; i < numberCities; i++)
+            if (location.equals(locationVector.get(i))) {
+                System.out.println("Error,this city exists!");
+                sw = 1;
+            }
+        if (sw == 0) {
+            vectId[numberCities] = numberCities;
+            locationVector.add(location);
+            numberCities++;
+        } else {
+            System.out.println("Cannot add the city");
+        }
+    }
+
+    public void PossibleCity(Location locationStart, Location locationFinish) {
+        int sw = 0;
+        for (int i = 0; i < roadVector.size(); i++) {
+            if (locationStart.getCoord().equals(roadVector.get(i).getFirstCity())) {
+                sw++;
+            }
+            if (locationFinish.getCoord().equals(roadVector.get(i).getSecondCity())) {
+                sw++;
+            }
+        }
+        if (sw == 2) {
+            System.out.println("It exists such a road!");
+        } else {
+            System.out.println("It doesn't exists such a road from the 2 cities!");
+        }
 
     }
-    public double optimalDist(Location location1,Location location2)
-    {
-          double min=10000;
 
-          return min;
-    }
-    public Map(){
-          roadsNumber=0;
-          numberCities=0;
+    public Map() {
+        roadsNumber = 0;
+        numberCities = 0;
     }
 }
--- Seminar2/Homework2/src/Main.java
+++ Seminar2/Homework2/src/Main.java
@@ -10,6 +10,8 @@ public class Main {
         Map map = new Map();
         Coordonates coordonates1=new Coordonates(12.0,3.6);
         Coordonates coordonates2=new Coordonates(13.0,4.6);
+        Coordonates coordonates3=new Coordonates(15.0,4.6);
+
         AirPort airPort = new AirPort(123);
         HighWay roadType = new HighWay("Express");
         StringBuilder stringBuilder = new StringBuilder("Bucuresti");
@@ -19,18 +21,27 @@ public class Main {
        // System.out.println(roads.toString());
         Location loc1 = new Location("Buc",roadType,coordonates1);
         Location loc2 = new Location("Iasi",roadType,coordonates2);
+        Location loc3 = new Location("Tulcea",roadType,coordonates3);
         Road road = new Road(coordonates1,coordonates2,143.5,10,roadType);
+       // Road road2 = new Road(coordonates1,coordonates3,143.5,10,roadType);
         map.addCity(loc1);
         map.addCity(loc2);
+        map.addCity(loc3);
         map.addRoad(road);
-       // System.out.println(map.locationVector.get(map.numberCities).toString());
-        /*  for(int i=0;i<=1;i++)
-            for(int j=0;j<=1;j++)
-            {
-                System.out.println(map.costMatrix[i][j]);
-            }
+      //  map.addRoad(road2);
+        //Testez Matricea
+          for(int i=0;i<=2;i++) {
+              for (int j = 0; j <= 2; j++) {
+                  System.out.print(map.costMatrix[i][j]);
+                  System.out.print(" ");
 
-       */
+              }
+              System.out.println("");
+          }
+         //Drum de la loc 1 la loc 2 exista :
+          map.PossibleCity(loc1,loc2);
+         //Drum de la loc 1 la loc 3 nu exista:
+          map.PossibleCity(loc1,loc3);
     }
 }
 
