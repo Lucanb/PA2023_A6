13f6936d29a8f413fc2bb0796e21e130b830bafd
Lucanb <66071357+Lucanb@users.noreply.github.com>
2023-03-14 15:06:12 +0200

    Edit

--- Seminar4/Compulsory4/src/Main.java
+++ Seminar4/Compulsory4/src/Main.java
@@ -1,5 +1,8 @@
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+      //  var students = IntStream.rangeClosed(0,3).mapToObj(i->new Student ("S"+i))).toArray(Student[]::new);
     }
 }
