cef7f39f3d847affc5e18f16815874e291e7e8c9
Lucanb <66071357+Lucanb@users.noreply.github.com>
2023-03-20 00:45:51 +0200

    addBonus

--- Seminar4/Homework4/src/main/java/org/example/Main.java
+++ Seminar4/Homework4/src/main/java/org/example/Main.java
@@ -9,20 +9,19 @@ public class Main {
         var students = IntStream.rangeClosed(0, 3).mapToObj(i -> new Students("S" + i)).toArray(Students[]::new);
         var projects = IntStream.rangeClosed(0, 3).mapToObj(i -> new Projects("P" + i)).toArray(Projects[]::new);
         List<Students> studentsList = new ArrayList<Students>();
-        TreeSet<Projects>setProjects = new TreeSet<Projects>();
+        TreeSet<Projects> setProjects = new TreeSet<Projects>();
         setProjects.addAll(Arrays.asList(projects));
-        for(Students s : students)
-        {
+        for (Students s : students) {
             studentsList.add(s);
         }
         //   studentsList.addAll(Arrays.asList(students)); //Sau asa;
-        Collections.sort(studentsList,Comparator.comparing(Students::getName));
+        Collections.sort(studentsList, Comparator.comparing(Students::getName));
 
-        for(Students s : students)
-        {
+        for (Students s : students) {
             System.out.println(s.getName());
         }
 
-           System.out.println(setProjects);
+        System.out.println(setProjects);
     }
 }
+
