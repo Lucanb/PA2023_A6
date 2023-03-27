a28317f7087aae393939089d8484c3480400f1f6
Lucanb <66071357+Lucanb@users.noreply.github.com>
2023-03-27 20:58:39 +0300

    Update for homework

---
+++ Seminar5/Homework5/src/main/java/org/example/Listt.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.List;
+
+public class Listt extends Command{
+    public static void list(Catalog catalog)
+    {
+        if(catalog.getDocs() != null)
+        {
+            for(Document iterator : catalog.getDocs())
+            {
+                System.out.print(iterator);
+            }
+        }
+        else
+        {
+            System.out.println(catalog.getName() + " doesn't have any documents.");
+        }
+    }
+}
--- Seminar5/Homework5/src/main/java/org/example/Main.java
+++ Seminar5/Homework5/src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private void testCreateSave() throws IOException, InvalidCommand {
+        
+        Catalog catalog = new Catalog("MyDocuments");
+        var book = new Document("article1","123"," RandomLocation1");
+        var article = new Document("book1","234","catalog.json");
+        
+        article.addTag("Title", "FCSB e Steaua");
+        article.addTag("Author", "George Stancu");
+
+        catalog.addDocument(book);
+        catalog.addDocument(article);
+
+        Save.save(catalog, "catalog.json");
+    }
+
+    private void testLoadView() throws IOException, InvalidCatalogException, TemplateException, URISyntaxException, InvalidCommand {
+        Catalog catalog = Load.load("catalog.json");
+        View.view(catalog.findById("234"));
+        Listt.list(catalog);
+        Report.report(catalog);
+    }
+    public static void main(String[] args) throws IOException, InvalidCatalogException, TemplateException, URISyntaxException, InvalidCommand {
+        Main app = new Main();
+        app.testCreateSave();
+        app.testLoadView();
+
     }
 }
