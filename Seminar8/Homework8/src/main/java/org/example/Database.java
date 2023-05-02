93e4118850e1ae37dcfc3184ad6bf555e52de5d0
Lucanb <66071357+Lucanb@users.noreply.github.com>
2023-05-02 14:55:19 +0300

    Update

--- Seminar8/Homework8/src/main/java/org/example/Database.java
+++ Seminar8/Homework8/src/main/java/org/example/Database.java
@@ -1,13 +1,32 @@
 package org.example;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Database {
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
     private static final String USER = "LAB8";
     private static final String PASSWORD = "sql";
     private static Connection connection = null;
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
 
+    static {
+        config.setJdbcUrl(URL);
+        config.setUsername(USER);
+        config.setPassword(PASSWORD);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
+    }
+
+    public static Connection getConnectionSv() throws SQLException {
+        return ds.getConnection();
+    }
 
     public static Connection getConnection() {
         return connection;
@@ -33,6 +52,7 @@ public class Database {
     public static void createConnection() {
         try {
             connection = java.sql.DriverManager.getConnection(getURL(), getUSER(), getPASSWORD());
+           // connection = getConnectionSv();
             connection.setAutoCommit(false);
         } catch (Exception e) {
             e.printStackTrace();
