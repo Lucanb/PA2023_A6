efdb702051b54e4adfe09a0b090d989966dee48f
Luca <lucanastasa@yahoo.com>
2023-05-24 23:47:24 +0300

    Pare ca merge

---
+++ Project/myreactfiles/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import DataTable from "react-data-table-component";
+import {useState,useEffect} from 'react';
+
+function App() {
+    const[data,setData]=useState([])
+    const[loading,setLoading] = useState(false)
+    const[perPage,setPerPage] = useState(10)
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+    async function fetchData(){
+        setLoading(true)
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+        const users = await response.json()
+        setData(users)
+        setLoading(false)
+    }
+    const columns=[
+        {
+            name: "Name",
+            selector: (row) => row.userId,
+        },
+        {
+            name: "Username",
+            selector: (row) => row.title,
+        },
+        {
+            name: "email",
+            selector: (row) => row.completed ? "completed" : "not completed",
+        },
+    ]
+   return (
+      <div className="App">
+         <DataTable
+         title="History"
+         columns={columns}
+         data={data}
+         progressPending={loading}
+         pagination={perPage}
+         />
+      </div>
+   )
+}
+export default App;
--- Project/myreactfiles/src/index.js
+++ Project/myreactfiles/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MyRouter from "./MyRouter";
+import App from "./App";
 
-ReactDOM.render(<MyRouter/>,document.getElementById("root"));
+ReactDOM.render(<App/>,document.getElementById("root"));
 
 
