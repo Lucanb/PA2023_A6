cd4904733d56289b6c3cf94d98eaeddd783988ca
Luca <lucanastasa@yahoo.com>
2023-05-30 21:38:15 +0300

    RecoveredFile
    Pierdusem fisierul la upload dar l-am recuperat

--- Project/Data_Base/src/main/java/com/example/DataBase_Accounts/SpecialClasses/Graphic.java
+++ Project/Data_Base/src/main/java/com/example/DataBase_Accounts/SpecialClasses/Graphic.java
@@ -10,37 +10,35 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class Graphic {
-    private final List<Callable<List<String>>> tasks = new ArrayList<>();
+    private final List<Callable<List<String>>> road = new ArrayList<>();
+    private final StringBuilder Myresult = new StringBuilder();
+    private final Set<String> VizNode = new HashSet<>();
     Graph<String, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
-    private final StringBuilder result = new StringBuilder();
-    private final Set<String> visitedNodes = new HashSet<>();
     public Graphic(List<Integer> Vertex, List<Pair<Integer, Integer>> edges) {
         for (int i = 0; i < Vertex.size(); i++) {
             graph.addVertex(Vertex.get(i).toString());
         }
+
         for (int j = 0; j < edges.size(); j++) {
             graph.addEdge(edges.get(j).getFirst().toString(), edges.get(j).getSecond().toString());
         }
 
     }
-
     public void DFS(String node) {
-        DepthFirstIterator<String, DefaultEdge> dfsIterator = new DepthFirstIterator<>(graph, node);
-        while (dfsIterator.hasNext()) {
-            String vertex = dfsIterator.next();
+        DepthFirstIterator<String, DefaultEdge> actualNode = new DepthFirstIterator<>(graph, node);
+        while (actualNode.hasNext()) {
+            String vertex = actualNode.next();
             System.out.println(vertex);
         }
     }
-
     public void StartParalelDfs(Integer threadsNumber) {
-        for (String vertex : graph.vertexSet()) {
-            tasks.add(() -> {
-                DepthFirstIterator<String, DefaultEdge> dfsIterator = new DepthFirstIterator<>(graph, vertex);
+        for (String vertex : graph.vertexSet()) {road.add(() -> {
+                DepthFirstIterator<String, DefaultEdge> actualNode = new DepthFirstIterator<>(graph, vertex);
                 List<String> traversalResult = new ArrayList<>();
-                while (dfsIterator.hasNext()) {
-                    String v = dfsIterator.next();
-                    if (!visitedNodes.contains(v)) {
-                        visitedNodes.add(v);
+                while (actualNode.hasNext()) {
+                    String v = actualNode.next();
+                    if (!VizNode.contains(v)) {
+                        VizNode.add(v);
                         traversalResult.add(v);
                     }
                 }
@@ -50,10 +48,10 @@ public class Graphic {
 
         ExecutorService executorService = Executors.newFixedThreadPool(threadsNumber);
         try {
-            List<Future<List<String>>> futures = executorService.invokeAll(tasks);
-            for (Future<List<String>> future : futures) {
-                List<String> traversalResult = future.get();
-                result.append(String.join(" ", traversalResult)).append(" ");
+            List<Future<List<String>>> nextNode = executorService.invokeAll(road);
+            for (Future<List<String>> next : nextNode) {
+                List<String> traversalResult = next.get();
+                Myresult.append(String.join(" ", traversalResult)).append(" ");
             }
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
@@ -61,7 +59,7 @@ public class Graphic {
 
         executorService.shutdown();
 
-        System.out.println(result.toString());
+        System.out.println(Myresult.toString());
     }
 
 }
