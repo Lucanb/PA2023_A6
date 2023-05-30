37dec2f7a1e080ccb30a25a656c8ef3769a3cbef
Nastasa Baras Luca <66071357+Lucanb@users.noreply.github.com>
2023-05-30 15:19:23 +0300

    Delete Project/myreactfiles directory

--- Project/myreactfiles/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import History from "./History";
-
-ReactDOM.render(<History/>,document.getElementById("root"));
-
-
